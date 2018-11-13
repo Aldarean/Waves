fed179d1b39eca11b065be3769fd895dc6bd20d6
Rodney Wormsbecher <rodney22@live.nl>
2018-11-13 23:26:45 +0100

    created register form layout

--- client/src/components/Register_login/register.js
+++ client/src/components/Register_login/register.js
@@ -16,7 +16,7 @@ class Register extends Component {
                 value: '',
                 config: {
                     name: 'name_input',
-                    type: 'name',
+                    type: 'text',
                     placeholder: 'Enter your name'
                 },
                 validation: {
@@ -26,12 +26,13 @@ class Register extends Component {
                 touched: false,
                 validationMessage: ''
             },
+            
             lastname: {
                 element: 'input',
                 value: '',
                 config: {
                     name: 'lastname_input',
-                    type: 'name',
+                    type: 'text',
                     placeholder: 'Enter your lastname'
                 },
                 validation: {
@@ -41,15 +42,156 @@ class Register extends Component {
                 touched: false,
                 validationMessage: ''
             },
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                validation: {
+                    required: true,
+                    email: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage: ''
+            },
+            password: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'password_input',
+                    type: 'password',
+                    placeholder: 'Enter your password'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage: ''
+            }, 
+            confirmPassword: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'confirm_password_input',
+                    type: 'password',
+                    placeholder: 'Confirm your password'
+                },
+                validation: {
+                    required: true,
+                    confirm: 'password'
+                },
+                valid: false,
+                touched: false,
+                validationMessage: ''
+            }
             
         }
+    }
+
+    updateForm = (element) => {
+        const newFormdata = update(element, this.state.formData, 'register');
+        this.setState({
+            formError: false,
+            formData: newFormdata
+        })
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+        let dataToSubmit = generateData(this.state.formData, 'register');
+        let formIsValid = isFormValid(this.state.formData, 'register')
+
+        if (formIsValid) {
+           console.log('data to submit: ', dataToSubmit);
+        } else {
+            this.setState({
+                formError: true
+            });
+        }
+        
     }
 
 
   render() {
     return (
-      <div>
-        register
+      <div className="page_wrapper">
+        <div className="container">
+            <div className="register_login_container">
+                
+                <div className="left">
+                    <form onSubmit={(event) => this.submitForm(event)}>
+                        <h2>Personal information</h2>
+                        <div className="form_block_two">
+                            <div className="block">
+                                <FormField 
+                                    id={'name'}
+                                    formData={this.state.formData.name}
+                                    change={ (element) => this.updateForm(element)}
+                                />
+                            </div>
+
+                             <div className="block">
+                                <FormField 
+                                    id={'lastname'}
+                                    formData={this.state.formData.lastname}
+                                    change={ (element) => this.updateForm(element)}
+                                />
+                            </div>
+
+                        </div>
+
+                        <div>
+                        <FormField 
+                            id={'email'}
+                            formData={this.state.formData.email}
+                            change={ (element) => this.updateForm(element)}
+                        />
+
+                        </div>
+
+                        <h2>Verify password</h2>
+                        <div className="form_block_two">
+                            <div className="block">
+                                <FormField 
+                                    id={'password'}
+                                    formData={this.state.formData.password}
+                                    change={ (element) => this.updateForm(element)}
+                                />
+                            </div>
+
+                             <div className="block">
+                                <FormField 
+                                    id={'confirmPassword'}
+                                    formData={this.state.formData.confirmPassword}
+                                    change={ (element) => this.updateForm(element)}
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            {this.state.formError ? 
+                            <div className="error_label">
+                                    Please check your data.
+                                </div>    
+                            : null }
+
+                            <button onClick={(event) => this.submitForm(event)}>
+                                Create an account
+                            </button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+
+            </div>
+        </div>
       </div>
     )
   }
